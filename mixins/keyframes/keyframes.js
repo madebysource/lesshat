10b529e4d82b390d6e1f76fbae90b5f5c7c56866
Petr Brzek <petrbrzek@abdoc.net>
2013-11-02 20:57:02 +0100

    recoding whole keyframe mixing

--- mixins/keyframes/keyframes.js
+++ mixins/keyframes/keyframes.js
@@ -3,101 +3,127 @@
  */
 
 var keyframes = function keyframes(value) {
+
+  // States
+  // 1 - all
+  // 2 - webkit, moz, w3c
+  // 3 - webkit, moz
+  // 4 - webkit, opera
+  // 5 - webkit, w3c
+  // 6 - webkit
+  // 7 - moz, opera, w3c
+  // 8 - moz, opera
+  // 9 - moz, w3c
+  // 10 - moz
+  // 11 - opera, w3c
+  // 12 - opera
+  // 13 - w3c
+
   value = value || 08121991;
+  var state = '@{state}';
+  var value_temp = value;
   if (value == 08121991) {
     return value;
   }
-  var selector = '@keyframes ';
-  var definition = value.split(',');
-  value = '0; } \n' + selector + definition[0] + '{' +  definition[1];
+  var prefixedProperties = ['animation', 'background-size', 'box-shadow', 'column', 'transform', 'filter'];
 
-  return value;
-};
-
-keyframes.webkit = function keyframes(value) {
-  value = value || 08121991;
-  if (value == 08121991) {
-    return value;
+  switch (state) {
+    case '1':
+      syntax('start', '@-webkit-keyframes', '-webkit-');
+      syntax(null, '@-moz-keyframes', '-moz-');
+      syntax(null, '@-o-keyframes', '-o-');
+      syntax('end', '@keyframes');
+      break;
+    case '2':
+      syntax('start', '@-webkit-keyframes', '-webkit-');
+      syntax(null, '@-moz-keyframes', '-moz-');
+      syntax('end', '@keyframes');
+      break;
+    case '3':
+      syntax('start', '@-webkit-keyframes', '-webkit-');
+      syntax('end', '@-moz-keyframes', '-moz-');
+      break;
+    case '4':
+      syntax('start', '@-webkit-keyframes', '-webkit-');
+      syntax('end', '@-o-keyframes', '-o-');
+      break;
+    case '5':
+      syntax('start', '@-webkit-keyframes', '-webkit-');
+      syntax('end', '@keyframes');
+      break;
+    case '6':
+      syntax('startend', '@-webkit-keyframes', '-webkit-');
+      break;
+    case '7':
+      syntax('start', '@-moz-keyframes', '-moz-');
+      syntax(null, '@-o-keyframes', '-o-');
+      syntax('end', '@keyframes');
+      break;
+    case '8':
+      syntax('start', '@-moz-keyframes', '-moz-');
+      syntax('end', '@-o-keyframes', '-o-');
+      break;
+    case '9':
+      syntax('start', '@-moz-keyframes', '-moz-');
+      syntax('end', '@keyframes');
+      break;
+    case '10':
+      syntax('startend', '@-moz-keyframes', '-moz-');
+      break;
+    case '11':
+      syntax('start', '@-o-keyframes', '-o-');
+      syntax('end', '@keyframes');
+      break;
+    case '12':
+      syntax('startend', '@-o-keyframes', '-o-');
+      break;
+    case '13':
+      syntax('startend', '@keyframes');
+      break;
   }
-  var prefixedProperties = ['animation','background-size', 'border-', 'box-shadow', 'column', 'transform', 'filter'];
-  var prefix = '-webkit-';
-  var selector = '@-webkit-keyframes ';
-  var definition = value.split(',');
 
-  prefixedProperties.forEach(function(property, index) {
-    if (value.indexOf(property) !== -1) {
-      definition[1] = definition[1].replace(new RegExp(property, 'g'), function(match) {
-        return prefix + match;
-      })
+  function syntax(start, selector, prefix) {
+    var end = '}\n';
+    var definition = value_temp.split(',');
+    var syntax = selector + ' ' + definition[0] + '{' + definition[1] + end;
+
+    if (prefix) {
+      prefixedProperties.forEach(function(property, index) {
+        if (value.indexOf(property) !== -1) {
+          definition[1] = definition[1].replace(new RegExp(property, 'g'), function(match) {
+            return prefix + match;
+          })
+        }
+      });
     }
-  });
 
-  value = '0; } \n' + selector + definition[0] + '{' +  definition[1];
-
-  return value;
-};
-
-keyframes.moz = function keyframes(value) {
-  value = value || 08121991;
-  if (value == 08121991) {
-    return value;
-  }
-  var prefixedProperties = ['animation','background-size', 'border-', 'box-shadow', 'column', 'transform', 'filter'];
-  var prefix = '-moz-';
-  var selector = '@-moz-keyframes ';
-  var definition = value.split(',');
-
-  prefixedProperties.forEach(function(property, index) {
-    if (value.indexOf(property) !== -1) {
-      definition[1] = definition[1].replace(new RegExp(property, 'g'), function(match) {
-        return prefix + match;
-      })
+    if (start == 'start') {
+      value = '0; } \n' + syntax;
+    } else if (start == 'startend') {
+      value = '0; } \n' + syntax.replace(end, '');
+    } else if (start == 'end') {
+      value = value + syntax.replace(end, '');
+    } else {
+      value = value + syntax;
     }
-  });
 
-  value = '0; } \n' + selector + definition[0] + '{' +  definition[1];
-
-  return value;
-};
-
-keyframes.opera = function keyframes(value) {
-  value = value || 08121991;
-  if (value == 08121991) {
-    return value;
   }
-  var prefixedProperties = ['animation','background-size', 'border-', 'box-shadow', 'column', 'transform', 'filter'];
-  var prefix = '-o-';
-  var selector = '@-o-keyframes ';
-  var definition = value.split(',');
-
-  prefixedProperties.forEach(function(property, index) {
-    if (value.indexOf(property) !== -1) {
-      definition[1] = definition[1].replace(new RegExp(property, 'g'), function(match) {
-        return prefix + match;
-      })
-    }
-  });
-
-  value = '0; } \n' + selector + definition[0] + '{' +  definition[1];
 
   return value;
 };
 
-keyframes.result = {
-  property: 'lesshat-selector { pb',
-  prefixed: false,
-  strip: ';'
-};
 
-keyframes.appendCSS = {
-  all: '}',
-}
+/**
+ * We want totally custom result definition, because keyframes mixin is tricky one (not recommended, it's not testable)
+ */
+
+keyframes.result = 'result.less';
 
 /**
  * For which browsers is this mixin specified
  */
 
-keyframes.vendors = ['webkit','moz','opera'];
+keyframes.vendors = ['webkit', 'moz', 'opera'];
 
 /**
  * Export mixin
