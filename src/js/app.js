4455818d7df88fe38a4c51e457589b4af3b68f2e
Petr Brzek <petrbrzek@abdoc.net>
2013-11-07 23:52:11 +0100

    Added 2.0.3 version

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ $(function () {
     var logo = $('#logo');
     var second = $('#second');
 
-    var zip = 'https://github.com/csshat/lesshat/archive/v2.0.2.zip';
+    var zip = 'https://github.com/csshat/lesshat/archive/v2.0.3.zip';
 
     var downloaded = false;
 
